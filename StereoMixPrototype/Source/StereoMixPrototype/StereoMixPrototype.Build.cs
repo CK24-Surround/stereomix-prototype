56c780962bdd2bbb987392438dd318ff4266a7a1
mintyway0112 <mintyway0112@gmail.com>
2024-02-29 13:04:09 +0900

    Init

---
+++ StereoMixPrototype/Source/StereoMixPrototype/StereoMixPrototype.Build.cs
@@ -0,0 +1,24 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class StereoMixPrototype : ModuleRules
+{
+	public StereoMixPrototype(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PublicIncludePaths.AddRange(new string[] { "StereoMixPrototype" });
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
